523b86242a80c8232dd1961548174d271c6387d1
lmlstudio <77842401+lmlstudio@users.noreply.github.com>
2021-05-09 10:08:26 +0700

    Update 118A - String Task.cpp

--- 118A - String Task.cpp
+++ 118A - String Task.cpp
@@ -10,7 +10,7 @@ int main()
     cin>>s;
     for(int i=0;i<=s.size()-1;i++)
     {
-		s[i]=tolower(s[i]);
+	s[i]=tolower(s[i]);
         if(s[i]!='a'&&s[i]!='e'&&s[i]!='i'&&s[i]!='o'&&s[i]!='u'&&s[i]!='y'&&s[i]!='A'&&s[i]!='E'&&s[i]!='O'&&s[i]!='I'&&s[i]!='U'&&s[i]!='Y')
         {
             cout<<"."<<s[i];
