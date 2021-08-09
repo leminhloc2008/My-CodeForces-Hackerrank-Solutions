718b80c513b3089d4cda614ec92cda9b216b6db0
lmlstudio <77842401+lmlstudio@users.noreply.github.com>
2021-08-09 18:56:53 +0700

    Create C-Nice Garland.cpp

---
+++ C-Nice Garland.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long n,kq=1e9,dem=0,luu;
+    string s;
+    cin>>n>>s;
+    string led[6]={"GRB", "GBR", "RGB", "RBG", "BGR", "BRG"};
+    string res;
+    for(auto i:led)
+    {
+        string leddep=i;
+        while(leddep.size()<n)
+        {
+            leddep=leddep+leddep;
+        }
+        int d=0;
+        for(int j=0;j<n;j++)
+        {
+            if(s[j]!=leddep[j])
+            {
+                d++;
+            }
+
+        }
+        if(kq>d)
+        {
+            kq=d;
+            res=leddep.substr(0,n);
+        }
+    }
+    cout<<kq<<endl<<res;
+    return 0;
+}
+
+
