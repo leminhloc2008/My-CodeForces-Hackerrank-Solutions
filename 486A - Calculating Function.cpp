f2683b0f71b44762f0d9fc854956f32929b12aac
lmlstudio <77842401+lmlstudio@users.noreply.github.com>
2021-08-28 17:11:23 +0700

    Update 486A - Calculating Function.cpp

--- 486A - Calculating Function.cpp
+++ 486A - Calculating Function.cpp
@@ -40,18 +40,9 @@ using namespace std;
 
 int  main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    long long n,SumOdd=0,SumEven=0;
+    long long n;
     cin>>n;
-    if(n%2==0)
-    {
-        cout<<n/2;
-    }
-    else
-    {
-        cout<<-(n+1)/2;
-    }
+    if(n%2==0) cout<<n/2;
+    else cout<<-(n+1)/2;
     return 0;
 }
