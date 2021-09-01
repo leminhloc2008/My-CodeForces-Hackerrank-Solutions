0d9895f4d67b8cf15fc47524b6d803efc7a8647e
lmlstudio <77842401+lmlstudio@users.noreply.github.com>
2021-09-01 18:14:44 +0700

    Create 1133C - Balanced Team.cpp

---
+++ 1133C - Balanced Team.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long n;
+    cin>>n;
+    long long a[n+1];
+    for(int i=1;i<=n;i++)
+    {
+        cin>>a[i];
+    }
+    sort(a+1,a+1+n);
+    long long dem=0,kq=0,j=1;
+    for(int i=1;i<=n;i++)
+    {
+        while(j<=n&&a[j]-a[i]<=5)
+        {
+            j++;
+            kq=max(kq,j-i);
+        }
+    }
+    cout<<kq;
+}
